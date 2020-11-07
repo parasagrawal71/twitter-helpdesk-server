b1d366b69e0c2e8ce73b095c461c8cfb097b6834
Paras Agrawal <paras@piktorlabs.com>
2020-11-07 23:52:39 +0530

    Update db password in heroku environment

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,4 +1,3 @@
 module.exports = {
-  MONGODB_URI:
-    "mongodb+srv://parasagrawal71:<DB_PASSWORD>@twitter-helpdesk-server.7cauu.mongodb.net/twitter-helpdesk?retryWrites=true&w=majority",
+  MONGODB_URI: `mongodb+srv://parasagrawal71:${process.env.USER_DB_PASSWORD}@twitter-helpdesk-server.7cauu.mongodb.net/twitter-helpdesk?retryWrites=true&w=majority`,
 };
