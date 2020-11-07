d2499ceda04fce7a3c99949ea3260aa5b46bf015
Paras Agrawal <paras@piktorlabs.com>
2020-11-08 00:16:59 +0530

    Generate random oauth nounce

--- src/api/auth/auth.controller.js
+++ src/api/auth/auth.controller.js
@@ -13,7 +13,7 @@ module.exports.requestToken = (req, res) => {
     .request({
       method: "POST",
       url: "https://api.twitter.com/oauth/request_token",
-      Authorization: `OAuth oauth_nonce="K7ny27JTpKVsTgdyLeDfmQQWVLELj2zAK5BslRsqyw", oauth_callback="http%3A%2F%2Flocalhost%3A3000%2F", oauth_signature_method="HMAC-SHA1", oauth_timestamp="${Math.floor(
+      Authorization: `OAuth oauth_nonce="${randomStr()}", oauth_callback="http%3A%2F%2Flocalhost%3A3000%2F", oauth_signature_method="HMAC-SHA1", oauth_timestamp="${Math.floor(
         new Date().getTime() / 1000
       )}", oauth_consumer_key="JAABlOt9wzw9dyr8SASkPjRrj", oauth_signature="ki0m1aFKtdYisdalDQUOHnfOS0EI5XC1Iez1xbhx0Htox2NwrI", oauth_version="1.0"`,
     })
@@ -25,4 +25,14 @@ module.exports.requestToken = (req, res) => {
       // console.log("Error: ", e);
       return failureResponse(res, e.message, e);
     });
+};
+
+const randomStr = () => {
+  const len = 50;
+  const arr = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let ans = "";
+  for (let i = len; i > 0; i--) {
+    ans += arr[Math.floor(Math.random() * arr.length)];
+  }
+  return ans;
 };
