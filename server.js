d72e8a788246278a78c73827a08a2f537bf9b917
Paras Agrawal <paras@piktorlabs.com>
2020-11-08 19:38:03 +0530

    Fix small bug

--- server.js
+++ server.js
@@ -4,13 +4,11 @@ const bodyParser = require("body-parser");
 const apiRouter = require("./src/api/api.router");
 require("./src/utils/db.connect");
 
-// Here we are using cors to access the APIs over cross origins
-app.use(cors());
-
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
+app.use(cors());
 app.use("/api/v1", apiRouter);
 
 app.get("/", (req, res) => {
