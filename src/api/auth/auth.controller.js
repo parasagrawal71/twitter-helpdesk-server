d95b3cbd7a2084aca59f99f50ec9cb894aeb532b
Paras Agrawal <paras@piktorlabs.com>
2020-11-08 19:25:25 +0530

    Update request

--- src/api/auth/auth.controller.js
+++ src/api/auth/auth.controller.js
@@ -9,34 +9,15 @@ const {
  * @description Function to request the token to twitter request_token endpoint
  */
 module.exports.requestToken = (req, res) => {
-  // axios
-  //   .request({
-  //     method: "POST",
-  //     url: "https://api.twitter.com/oauth/request_token",
-  //     Authorization: `OAuth oauth_nonce="${randomStr()}", oauth_callback="${encodeURIComponent(
-  //       "https://twitter--helpdesk.herokuapp.com"
-  //     )}", oauth_signature_method="HMAC-SHA1", oauth_timestamp="${Math.floor(
-  //       Date.now() / 1000
-  //     )}", oauth_consumer_key="ywkvzrkLoWlJBDu1yYvBOgywg", oauth_signature="IPkHRKPkxx35K5Xgjf5173oHdJRaUyjZEf1xAocmPVhRsvwBNA", oauth_version="1.0"`,
-  //   })
-  //   .then((response) => {
-  //     // console.log("response: ", response);
-  //     return successResponse(res, "Request Token", response);
-  //   })
-  //   .catch((e) => {
-  //     // console.log("Error: ", e);
-  //     return failureResponse(res, e.message, e);
-  //   });
-
   const config = {
     method: "post",
     url: "https://api.twitter.com/oauth/request_token",
     headers: {
-      Authorization: `OAuth oauth_consumer_key="ywkvzrkLoWlJBDu1yYvBOgywg",oauth_signature_method="HMAC-SHA1",oauth_timestamp="${Math.floor(
+      Authorization: `OAuth oauth_consumer_key="ywkvzrkLoWlJBDu1yYvBOgywg", oauth_signature_method="HMAC-SHA1", oauth_timestamp="${Math.floor(
         Date.now() / 1000
-      )}",oauth_nonce="${randomStr()}",oauth_version="1.0",oauth_callback="${encodeURIComponent(
+      )}", oauth_nonce="${randomStr()}", oauth_version="1.0", oauth_callback="${encodeURIComponent(
         req.query.origin
-      )}",oauth_signature="IPkHRKPkxx35K5Xgjf5173oHdJRaUyjZEf1xAocmPVhRsvwBNA"`,
+      )}", oauth_signature="IPkHRKPkxx35K5Xgjf5173oHdJRaUyjZEf1xAocmPVhRsvwBNA"`,
     },
   };
 
