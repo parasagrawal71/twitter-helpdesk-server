00eee66727a4fee872d9615ea92f4d226c8c9c09
Paras Agrawal <paras@piktorlabs.com>
2020-11-08 19:34:44 +0530

    Add cors to app

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const apiRouter = require("./src/api/api.router");
 require("./src/utils/db.connect");
+
+// Here we are using cors to access the APIs over cross origins
+app.use(cors());
 
 const app = express();
 const PORT = process.env.PORT || 5000;
