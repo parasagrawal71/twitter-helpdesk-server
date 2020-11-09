f0793e6f77c41138677345540c0bed5462ee844d
Paras Agrawal <paras@piktorlabs.com>
2020-11-10 03:47:40 +0530

    Update mentions endpoint

--- src/api/tweets/tweets.controller.js
+++ src/api/tweets/tweets.controller.js
@@ -9,20 +9,15 @@ const { requestTwitter } = require("../../utils/requestTwitter");
  * @function fetchMentions
  * @description Function to fetch mentions for current user
  */
-module.exports.fetchMentions = (req, res) => {
-  requestTwitter(
+module.exports.fetchMentions = async (req, res) => {
+  const result = await requestTwitter(
     req,
     "get",
     "https://api.twitter.com/1.1/statuses/mentions_timeline.json"
-  )
-    .then((response) => {
-      return successResponse(
-        res,
-        "List of mentions",
-        response && response.data
-      );
-    })
-    .catch((e) => {
-      return failureResponse(res, e.message, e);
-    });
+  );
+
+  if (result && result.success) {
+    return successResponse(res, "List of mentions", result && result.data);
+  }
+  return failureResponse(res, result.message, result);
 };
