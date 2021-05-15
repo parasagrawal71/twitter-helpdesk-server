dde6a8382fc28eb9c76d944d2644beaf8ab61918
Paras Agrawal <parasagrawal71@gmail.com>
2021-05-15 19:08:14 +0530

    Update db.config.js

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,5 +1,3 @@
 module.exports = {
-  MONGODB_URI: `mongodb+srv://parasagrawal71:${
-    process.env.USER_DB_PASSWORD || "twitter-password-123"
-  }@twitter-helpdesk-server.7cauu.mongodb.net/twitter-helpdesk?retryWrites=true&w=majority`,
+  LOCAL_MONGODB: "mongodb://localhost:27017/twitter-helpdesk",
 };
