60d079d787a39b2cd03bb395d940f16a7863a273
Paras Agrawal <parasagrawal71@gmail.com>
2023-02-16 22:16:36 +0530

    Change PORT to 6001

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const apiRouter = require("./src/api/api.router");
 require("./src/utils/db.connect");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6001;
 
 app.use(bodyParser.json());
 app.use(cors());
